332336b24e6ae5328a74c4cea515dc87921a711a
nminchow <noelminchow@gmail.com>
2021-06-24 02:03:50 -0400

    tweak build to compress element plus

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('minimal')) {
+            return 'card-data';
+          }
+          if (id.includes('element-plus')) {
+            return 'element-plus'
+          }
+        },
+      }
+    }
+  }
 })
